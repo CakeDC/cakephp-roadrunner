8e281f043692fe1d4b34802a8cb8f0f188762902
Ramon Dantas <ramon@deliverydireto.com.br>
2023-05-29 14:38:17 -0300

    chore: atualizar versões do PHP, Roadrunner e CakePHP

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c935f5e0add2cf0ae650d072c8357533e21b0c35.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c8a17ce7abc03c50cd072e9e6c9b389c5f61836b.tar.gz") {} }:
 with pkgs; let
-    php = php80.withExtensions ({ enabled, all }: enabled);
+    php = php81.withExtensions ({ enabled, all }: enabled);
 in
     mkShell {
         buildInputs = [
